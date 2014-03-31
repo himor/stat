ed8f1b887e343a2f74639794ec30583d765c7ed8
Mike Gordo <himor.cre@gmail.com>
2014-03-31 06:28:30 -0400

    updated assets definition

--- app/views/layout/layout.blade.php
+++ app/views/layout/layout.blade.php
@@ -3,11 +3,11 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Stat</title>
-    <link rel="stylesheet" type="text/css" href="/css/style.css">
-    <link rel="stylesheet" type="text/css" href="/css/table.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/table.css') }}">
 
     <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
-    <script src="/js/script.js"></script>
+    <script src="{{ asset('js/script.js') }}"></script>
 </head>
 <body>
     <div class="content">
